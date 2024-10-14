bf7e1ab141f1c0c34d59e27fa10b00fa32ecd932
jtacm1 <git@github.com:jtacm1>
2024-10-14 18:15:36 +0800

    测试linux共享内存的使用

--- fileops-kprobe.c
+++ fileops-kprobe.c
@@ -12,13 +12,8 @@
 
 #define pr_fmt(fmt) "%s: " fmt, __func__
 
-#include <linux/kernel.h>
-#include <linux/module.h>
-#include <linux/kprobes.h>
-// #include <linux/kallsyms.h>
-// #include <linux/fs.h>
+#include "./util.h"
 
-#define DEFAULT_RET_STR "-2"
 #define MAX_SYMBOL_LEN	64
 static char symbol[MAX_SYMBOL_LEN] = "vfs_write";
 module_param_string(symbol, symbol, sizeof(symbol), 0644);
@@ -27,59 +22,83 @@ module_param_string(symbol, symbol, sizeof(symbol), 0644);
 static struct kprobe kp = {
 	.symbol_name	= symbol,
 };
-struct event_context {
-	char * process_name;	//进程名
-	char * process_parent; //父进程名
-	const char * access_op;	//访问操作
-	const char * file_name;     //文件名
-	char * file_path;     //文件路径
-	size_t size;  //文件大小
-	unsigned long inode;  //文件inode
-	s64 access_time; //访问时间
-} event_context;
+
+static char *f_d_path(const struct path *path, char *buf, int buflen)
+{
+	char *name = DEFAULT_RET_STR;
+	if (buf) {
+		name = d_path(path, buf, buflen);
+		if (IS_ERR(name))
+			name = NAME_TOO_LONG;
+	}
+	return name;
+}
+
 /* kprobe pre_handler: called just before the probed instruction is executed */
 static int __kprobes write_handler_pre(struct kprobe *p, struct pt_regs *regs)
 {
-	const struct file *file = (struct file *)regs->di;//因为x86的参数传递规则是di，si，dx，cx，r8，r9，所以di就是vfs_write的第一个参数。arm默认是r0，r1，r2，r3，相应的取r0
+	const struct file *file = (struct file *)regs_get_arg1(regs);//因为x86的参数传递规则是di，si，dx，cx，r8，r9，所以di就是vfs_write的第一个参数。arm默认是r0，r1，r2，r3，相应的取r0
     static struct event_context *event;
-	char *buf;
-    size_t size = 0;
-	char *filepath = DEFAULT_RET_STR;
+	const char __user *buf;
+	char *kbuf = NULL;
+	char *pname_buf = NULL;
+	char *file_path = DEFAULT_RET_STR;
+	size_t size;
 	//pr_info("<%s> p->addr = 0x%p, ip = %lx, flags = 0x%lx\n",
 	//	p->symbol_name, p->addr, regs->ip, regs->flags);
-	buf = kmalloc(PATH_MAX, GFP_ATOMIC);
-	size = regs->dx;//vfs_write的第三个参数在dx中
+	buf = (const char __user *)regs_get_arg2(regs); //vfs_write的第二个参数在si中
+	size = (size_t)regs_get_arg3(regs);//vfs_write的第三个参数在dx中
     //pr_info("process = %s , pid = %d, process_parent = %s, file = %s, size = %ld\n" , current->comm, current->pid, current->real_parent->comm, file->f_path.dentry->d_name.name, size);	
-	event = kmalloc(sizeof(struct event_context), GFP_ATOMIC);
+
+	if (size <=0 || !S_ISREG(file_inode(file)->i_mode)) //判断是否为文件
+		return 0;
+	kbuf = f_kzalloc(size, GFP_ATOMIC);
+	pname_buf = kzalloc(PATH_MAX, GFP_ATOMIC);
+	if (!kbuf)
+		goto out;
+
+	if (f_copy_from_user(kbuf, buf, size))
+		goto out;
+
+
+	file_path = f_d_path(&(file->f_path), pname_buf, PATH_MAX); //获取文件路径
+
+
+	event = kzalloc(sizeof(struct event_context), GFP_ATOMIC);
+	if (!event)
+		goto out;
 	event->process_name = current->comm; //记录进程名
 	event->process_parent = current->real_parent->comm; //记录父进程名
 	event->access_op = kp.symbol_name;
 	event->file_name = file->f_path.dentry->d_name.name; //记录文件名
-	filepath = d_path(&(file->f_path), buf, PATH_MAX); //记录文件路径
-	event->file_path = filepath;
+	event->file_path = file_path;
 	event->size = size; //记录文件大小
-	if (size <= 0 && !S_ISREG(file_inode(file)->i_mode))
-		return 0;
+
 	
 	event->inode = (file->f_inode)->i_ino; //记录文件inode
 	event->access_time = (s64)ktime_get_real_seconds(); //记录访问时间
-	pr_info("process_name:%s\tprocess_parent:%s\tfile_op: %s\tfile_name:%s\tfile_path:%s\tsize:%ld\tinode:%lu\taccess_time:%lld\t\n", 
+	pr_info("process_name:%s\tprocess_parent:%s\tfile_op: %s\tfile_name:%p\tfile_path:%s\tsize:%ld\tinode:%lu\taccess_time:%lld\t\n",
 	event->process_name, event->process_parent, event->access_op, event->file_name, event->file_path, event->size, event->inode, event->access_time);
+	kfree(event);
+
+out:
+	if (kbuf)
+		kfree(kbuf);
+	if (pname_buf)
+		kfree(pname_buf);
+
+
+
 	return 0;
 }
 
 /* kprobe post_handler: called after the probed instruction is executed */
-static void __kprobes write_handler_post(struct kprobe *p, struct pt_regs *regs,
-				unsigned long flags)
-{
 
-}
 
 static int __init kprobe_init(void)
 {
 	int ret;
 	kp.pre_handler = write_handler_pre;
-	kp.post_handler = write_handler_post;
 
 	ret = register_kprobe(&kp);
 	if (ret < 0) {
---
+++ util.h
@@ -0,0 +1,122 @@
+//
+// Created by jt on 2024/10/12.
+//
+
+#ifndef UTIL_H
+#define UTIL_H
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/types.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/kprobes.h>
+#include <linux/fs.h>
+
+#define DEFAULT_RET_STR "-2"
+#define NAME_TOO_LONG "-4"
+
+struct event_context {
+    char * process_name;	//进程名
+    char * process_parent; //父进程名
+    const char * access_op;	//访问操作
+    const unsigned char * file_name;     //文件名
+    char * file_path;     //文件路径
+    size_t size;  //文件大小
+    unsigned long inode;  //文件inode
+    s64 access_time; //访问时间
+} event_context;
+
+#define f_kmalloc(size, flags) kmalloc(size, flags | __GFP_NOWARN)
+#define f_kzalloc(size, flags) kzalloc(size, flags | __GFP_NOWARN)
+#define f_kfree(ptr) while (0) { void * _ptr; if (_ptr) kfree(ptr);}
+
+
+static __always_inline unsigned long __must_check f_copy_from_user(void *to, const void __user *from, unsigned long n)
+{
+    unsigned long res;
+    preempt_disable();
+    /* validate user-mode buffer: ['from' - 'from' + 'n') */
+    if (access_ok(from, n))
+        res = __copy_from_user_inatomic(to, from, n);
+    else
+        res = n;
+    preempt_enable();
+    return res;
+}
+
+
+static inline unsigned long regs_get_arg1(struct pt_regs *regs) {
+    return regs->di;
+}
+
+static inline unsigned long regs_get_arg2(struct pt_regs *regs) {
+    return regs->si;
+}
+
+static inline unsigned long regs_get_arg3(struct pt_regs *regs) {
+    return regs->dx;
+}
+
+static inline unsigned long regs_get_arg4(struct pt_regs *regs) {
+    return regs->cx;
+}
+
+static inline unsigned long regs_get_arg5(struct pt_regs *regs) {
+    return regs->r8;
+}
+
+static inline unsigned long regs_get_arg6(struct pt_regs *regs) {
+    return regs->r9;
+}
+
+
+static inline unsigned long regs_get_arg1_syscall(struct pt_regs *regs) {
+    return regs->di;
+}
+
+static inline unsigned long regs_get_arg2_syscall(struct pt_regs *regs) {
+    return regs->si;
+}
+
+static inline unsigned long regs_get_arg3_syscall(struct pt_regs *regs) {
+    return regs->dx;
+}
+
+static inline unsigned long regs_get_arg4_syscall(struct pt_regs *regs) {
+    return regs->r10;
+}
+
+static inline unsigned long regs_get_arg5_syscall(struct pt_regs *regs) {
+    return regs->r8;
+}
+
+static inline unsigned long regs_get_arg6_syscall(struct pt_regs *regs) {
+    return regs->r9;
+}
+
+// Only Syscall Functions Parameter Can Use get_arg()
+static inline unsigned long get_arg1_syscall(struct pt_regs *regs) {
+    return regs_get_arg1_syscall((struct pt_regs *)regs_get_arg1_syscall(regs));
+}
+
+static inline unsigned long get_arg2_syscall(struct pt_regs *regs) {
+    return regs_get_arg2_syscall((struct pt_regs *)regs_get_arg1_syscall(regs));
+}
+
+static inline unsigned long get_arg3_syscall(struct pt_regs *regs) {
+    return regs_get_arg3_syscall((struct pt_regs *)regs_get_arg1_syscall(regs));
+}
+
+static inline unsigned long get_arg4_syscall(struct pt_regs *regs) {
+    return regs_get_arg4_syscall((struct pt_regs *)regs_get_arg1_syscall(regs));
+}
+
+static inline unsigned long get_arg5_syscall(struct pt_regs *regs) {
+    return regs_get_arg5_syscall((struct pt_regs *)regs_get_arg1_syscall(regs));
+}
+
+static inline unsigned long get_arg6_syscall(struct pt_regs *regs) {
+    return regs_get_arg6_syscall((struct pt_regs *)regs_get_arg1_syscall(regs));
+}
+
+#endif //UTIL_H
